b1b278d7d3b3835fd984bcc4207e5318a0ab01ab
CODE-WITH-PRASANTA <prasantakhuntia2018@gmail.com>
2024-03-29 13:06:08 +0530

    adding log in sign up page and some others

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Css/LoginSignup.css'
 
 const LoginSignup = () => {
     return (
-        <div>
+        <div className='LoginSignup'>
+            <div className="LoginSignup-container">
+                <h1>Sign Up</h1>
+                <div className="LoginSignup-Fields">
+                    <input type="Text" placeholder='Your Name' />
+                    <input type="Email" placeholder='Your Email' />
+                    <input type="Password" placeholder='Password' />
+                </div>
 
+                <div className="LoginSignup-Container">
+                    <button>Continue</button>
+                </div>
+                <p className='LogiinSignupLogin'>Already have an account ? <span>Login here</span></p>
+
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name='' id='' />
+                    <p>By continuing , i agree to the term of use & Privacy ppolicy</p>
+                </div>
+            </div>
         </div>
     )
 }
